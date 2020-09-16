c7c69cc8c9ef852b200d5bf39c37942534b62880
diego_alexander <diegocaste7@hotmail.com>
2020-09-16 13:59:50 -0500

    COMMIT
    tipo: new functions
    modulo: main
    autor: Diego Castellanos
    descripcion: se agrega codigo al inicio para permitir consultas a http
    cambios en base de datos: no hay cambios

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node API
+
+this is an example about an API made with Node JS
+
+## Requeriments
+
+* node JS
+* express
+* MySQL
+* body-parser
+* nodemon
--- app.js
+++ app.js
@@ -7,7 +7,26 @@ const PORT = process.env.PORT || 3050;
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    bodyParser.json();
+    // Pass to next layer of middleware
+    next();
+
+});
 
 //mysql
 const conn = mysql.createConnection({
@@ -71,7 +90,7 @@ app.get('/user/:id', (req, res) => {
         if (result.length > 0) {
             res.json(result);
         } else {
-            res.send("no users with internal id!");
+            res.json({ note: 'no found!' });
         }
     })
 });
